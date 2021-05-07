100e788bbd8806f727422e430db9123f5dce2460
Sujhan Ghimire <sujhanghimire@hotmail.com>
2021-05-07 16:33:14 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, function () {
+    console.log("server started at 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
