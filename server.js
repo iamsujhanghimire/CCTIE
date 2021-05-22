e54fac7b3f43d601e53ac7ae4ec94d920093d701
Dilasha Shrestha <dilasha.shrestha.ds@gmail.com>
2021-05-22 12:44:54 -0400

    Added login and register pages

--- server.js
+++ server.js
@@ -13,3 +13,89 @@ app.listen(3000, function () {
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get('/register', (req, res) => {
+    if (req.query.error) {
+        res.redirect("/register.html?error=" + req.query.error);
+    } else {
+        res.redirect("/register.html");
+    }
+});
+
+app.post('/register', (req, res) => {
+    const newUser = {
+        username: req.body.username,
+        fullname: req.body.fullname,
+        // profile: req.body.profile,
+        // brand: req.body.brand
+    };
+
+    User.register(
+        newUser,
+        req.body.password,
+        function (err, user) {
+            if(req.body.password!== req.body.confirm){
+                console.log(err);
+                res.redirect("/register?error= Password must match" );
+            }
+            else{
+                const authenticate = passport.authenticate("local");
+                authenticate(req, res, function () {
+                    res.redirect("/")
+                });
+            }
+            if (err) {
+                res.redirect("/register?error=" + err);
+
+
+            } else {
+                //write into cookies, authenticate the requests
+                const authenticate = passport.authenticate("local");
+                authenticate(req, res, function () {
+                    res.redirect("/")
+                });
+            }
+        }
+    );
+
+});
+
+
+app.get('/login', (req, res) => {
+    if (req.query.error) {
+        res.redirect("/login.html?error=" + req.query.error);
+    } else {
+        res.redirect("/login.html");
+    }
+});
+
+app.post('/login', (req, res) => {
+    const user=new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+    req.login(
+        user,
+        function(err){
+            if(err){
+                console.log(err);
+                res.redirect('login?error=Invalid username or password');
+            }else{
+                const authenticate = passport.authenticate(
+                    "local",
+                    {
+                        successRedirect:"/",
+                        failureRedirect:"/login?error=Username and password don't match"
+                    })
+                authenticate(req, res);
+            }
+
+        }
+    )
+});
+
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
