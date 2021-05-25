b895f102ad4f4f526af7e274864a8e7b98ac7c1b
Sujhan Ghimire <sujhanghimire@hotmail.com>
2021-05-25 16:51:31 -0400

    jQueried eboard page and changed eboard.css

--- public/js/eboard.js
+++ public/js/eboard.js
@@ -6,87 +6,32 @@ $.ajax({
 function processData(raw) {
     const data = Papa.parse(raw.trim(), {header: true}).data;
     console.log(data);
-    const memberListRoot = document.querySelector('#member_list');
-    memberListRoot.innerHTML = '';
-
+    // Adding UL and List Element
+    $('#members').append(`<ul class = "membersList"></ul>`)
     for (const member of data) {
-        console.log(member.name);
-        const li = document.createElement('li');
-        li.classList.add("list-group-item");
-        memberListRoot.appendChild(li);
-
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('row');
-        li.appendChild(rowDiv);
-
-        //Div for Pictures
-        const imgDiv = document.createElement('div');
-        imgDiv.classList.add('col-lg-3');
-        rowDiv.appendChild(imgDiv);
-
-        //Div for Member Information
-        const infoDiv = document.createElement('div');
-        infoDiv.classList.add('col-lg-9')
-        infoDiv.classList.add('ebInfo');
-        rowDiv.appendChild(infoDiv);
-
-        //Member Picture
-        const img = document.createElement('img');
-        img.setAttribute('src', member.picture);
-        imgDiv.appendChild(img);
-
-        //Member Name
-        const name = document.createElement('h1');
-        name.innerText = member.first_name + " " + member.last_name;
-        infoDiv.appendChild(name);
-
-        //Member Club Title
-        const major = document.createElement('h4');
-        major.innerHTML = member.major;
-        infoDiv.appendChild(major);
-
-        //Member Major
-        const address = document.createElement('h4');
-        address.innerText = "Major: " + member.address;
-        infoDiv.appendChild(address);
-
-        //Member Area of Interest
-        const interests = document.createElement('h4');
-        interests.innerText = "Area of Interest: " + member.interests;
-        infoDiv.appendChild(interests);
-
-        //Member LinkedIn
-        const icons = document.createElement('a');
-        icons.innerHTML = `<i class="fab fa-linkedin" onclick="" style="width: 100px"></i>`
-        infoDiv.appendChild(icons)
-
-        const linkedin = document.createElement('h4');
-        linkedin.innerHTML = `<a href=member.linkedin>LinkedIn</a>`
-        infoDiv.appendChild(linkedin);
-
+        console.log(member)
+        $('.membersList')
+            .append(`<li class = 'memberInfo list-group-item'></li>`)
     }
 
-    document.querySelectorAll('#member_list h1').forEach(ele => {
-        ele.classList.add('name_text');
-    });
+    // Filling LI with information and pictures
 
-    document.querySelector('#member_list').querySelectorAll('h4,p').forEach(ele => {
-        ele.classList.add("main_text");
-    });
-
-    const allOverview = document.querySelectorAll('#member_list p');
-    allOverview.forEach(function (p) {
-        p.style.fontSize = '1.2rem';
-    });
-
-    document.querySelector('#member_list').querySelectorAll('h5,p').forEach(ele => {
-        ele.classList.add("pru_noto");
-        ele.classList.add("larger_text");
-    });
-
+    $('.memberInfo').append(`<div class = "row"></div>`)
+    $('.memberInfo .row').append(`<div class ="col-lg-3 imgDiv"></div> <div class ="col-lg-9 infoDiv"></div>`)
+    $('.imgDiv').append(`<img class = "memberPic" alt = "..."/>`)
+    $('.memberPic').attr('src', function (idx){
+        return data[idx].picture;
+    })
+    $('.infoDiv')
+        .append(function (idx){
+            return `<h3 class="memberName">${data[idx].first_name} ${data[idx].last_name}</h3>`})
+        .append(function (idx){
+            return `<p class = "memberPos">${data[idx].position}</p>`})
+        .append(function (idx){
+            return `<p class = memberMajor><strong>Major:</strong> ${data[idx].major}</p>`})
+        .append(function (idx){
+            return `<p class = memberInt><strong>Interests:</strong> ${data[idx].interests}</p>`})
 }
-// $('.ebInfo a').append(`<i class="fab fa-linkedin"></i>`)
-
 
 $(document).ready(function (){
     $.getJSON('/get_current_user').done(function (data) {
